2ee959223f5a4cdba62c30ec4b020af1b2a40ac9
anishSharan15 <108509723+anishSharan15@users.noreply.github.com>
2026-02-16 12:28:04 +0530

    Implement basic arithmetic functions in test.py
    Added basic arithmetic functions including add, subtract, multiply, divide, and power. Included print statements for testing.

---
+++ test.py
@@ -0,0 +1,23 @@
+def add(a,b):
+    return a+b
+
+
+def subtract(a, b):
+    return a -b
+
+
+def multiply(a, b):
+    result = a * b
+    return result
+
+
+def divide(a, b):
+    return a / b
+
+
+def power(a,b):
+    return a**b
+
+
+print(add(10,5))
+print(divide(10,0))
